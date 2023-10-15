d37de71043b4b136f01da173c91f340ebb479dc3
Vasas Agoston <begoston@gmail.com>
2023-10-15 22:19:19 +0200

    Updated 10 number system to 2 number system

--- Consol_Apps.py
+++ Consol_Apps.py
@@ -1,12 +1,25 @@
 
 #10 number system to 2 number sys
-x = int(input("Adj meg egy számot 0-255 között: "))
+def TwoNumSys():
+    x = int(input("Adj meg egy számot 0-255 között: "))
+    y = []
+    if x >= 0 and x <= 255:
+        while 0 < x:
+            if x % 2 == 0:
+                print(f"{x} / 2 = 0")
+                x //= 2
+                y.append(0)
+            else:
+                print(f"{x} / 2 = 1")
+                x -= 1
+                x //= 2
+                y.append(1)
 
-while x > 0:
-    if x % 2 == 0:
-        print(f"{x} / 2 = 0")
-        x //= 2
+        while 8 - len(y) > 0:
+            y.append(0)
+        y.reverse()
+        print(y)
     else:
-        print(f"{x} / 2 = 1")
-        x -= 1
-        x //= 2
+        print("Csak 0 és 255 között.")
+
+TwoNumSys()
